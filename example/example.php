faf89e23d2ccdf299b690ffc84a48c28514ec047
João Paulo Luiz Lopes <joaopaulo.luizlopes@outlook.com>
2020-06-02 10:58:23 -0300

    Cria um arquivo de exemplos

---
+++ example/example.php
@@ -0,0 +1,29 @@
+<?php
+
+require_once '../vendor/autoload.php';
+
+use APICEP\ZipCode;
+
+$code = '06233030';
+$zipcode = new ZipCode($code);
+
+echo "Procurando pelo CEP $code:\n\n";
+
+if ($zipcode->wasFound()) {
+    echo "Endereço encontrado: \n\n";
+    echo "CEP...: {$zipcode->code}\n";
+    echo "Estado: {$zipcode->state}\n";
+    echo "Cidade: {$zipcode->city}\n";
+    echo "Bairro: {$zipcode->district}\n";
+    echo "Rua...: {$zipcode->address}\n\n";
+    echo "Por extenso: $zipcode\n\n";
+} else {
+    echo "O CEP $code não foi encontrado!\n\n";
+}
+
+$code = strval(rand(0, 9999));
+$zipcode = new ZipCode($code);
+
+if ($zipcode->isInvalid()) {
+    echo "O CEP $code é inválido:\n";
+}

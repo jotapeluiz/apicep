823aa713d51051f75f700d297666093b275650aa
João Paulo Luiz Lopes <joaopaulo.luizlopes@outlook.com>
2020-06-01 23:34:11 -0300

    Renomeia método

--- src/Helpers/ZipCodeHelper.php
+++ src/Helpers/ZipCodeHelper.php
@@ -4,7 +4,7 @@ namespace APICEP\Helpers;
 
 final class ZipCodeHelper
 {
-    public static function valid(string $zipcode): bool
+    public static function isValid(string $zipcode): bool
     {        
         return (preg_match('/^[0-9]{8}$/m', preg_replace('/[\D]/m', '', $zipcode)) === 1);
     }
--- tests/Helpers/ZipCodeHelperTest.php
+++ tests/Helpers/ZipCodeHelperTest.php
@@ -7,16 +7,16 @@ class ZipCodeHelperTest extends TestCase
 {
     public function testWhenPassingAZipCodeMustReturnWhichIsValid()
     {        
-        $this->assertTrue(ZipCodeHelper::valid('99999-999'));
-        $this->assertTrue(ZipCodeHelper::valid('99999999'));
+        $this->assertTrue(ZipCodeHelper::isValid('99999-999'));
+        $this->assertTrue(ZipCodeHelper::isValid('99999999'));
     }
 
     public function testWhenPassingAnInvalidZipCodeShouldReturnFalse()
     {        
-        $this->assertFalse(ZipCodeHelper::valid('99999'));
-        $this->assertFalse(ZipCodeHelper::valid('999999999'));
-        $this->assertFalse(ZipCodeHelper::valid('lorem ipsum'));
-        $this->assertFalse(ZipCodeHelper::valid(''));
-        $this->assertFalse(ZipCodeHelper::valid(' '));
+        $this->assertFalse(ZipCodeHelper::isValid('99999'));
+        $this->assertFalse(ZipCodeHelper::isValid('999999999'));
+        $this->assertFalse(ZipCodeHelper::isValid('lorem ipsum'));
+        $this->assertFalse(ZipCodeHelper::isValid(''));
+        $this->assertFalse(ZipCodeHelper::isValid(' '));
     }
 }
